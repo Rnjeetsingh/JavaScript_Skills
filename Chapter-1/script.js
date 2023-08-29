0b18b727e90cda3d9b1a8b3d8b65f05e69dd4557
Ranjeet Singh <107486727+Rnjeetsingh@users.noreply.github.com>
2023-08-29 23:45:34 +0530

    javascript topics update

---
+++ Chapter-1/script.js
@@ -0,0 +1,70 @@
+// let name = 'RanjeetSingh';
+// let pass = [1,2,3,4,5,6,7];
+
+// if(name.length < 8){
+//     if(pass.lenght < 8){
+//         console.log("please Enter 8 Character password")
+//     }
+// }else{
+//     console.log('Scussfull login')
+// }
+// console.log(name.length);
+// console.log(pass.length);
+
+
+// Switch Case
+
+// let ch = 10;
+
+// switch(ch){
+//     case 3: console.log('No 3');
+//     break;
+//     case 4: console.log('No 4');
+//     break;
+//     case 5: console.log('No 5');
+//     break;
+//     case 6: console.log('No 6');
+//     default:console.log('you prees wrong')
+// }
+
+
+// let i = 1;
+
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 2;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10);
+
+// (function(){
+//     console.log('Hello Ranjeet Singh')
+// })();
+
+
+// let arr = [2,5,6,4,7,8,9,7,5];
+// let b = arr.splice(1,7);
+// let b = arr.slice(1,7);
+// console.log(b);
+// let a = arr.map((e) => {
+    // console.log(e,el,err)
+    // return (e);
+// })
+
+// console.log(a.sort());
+// console.log(arr);
+// console.log(arr.slice(1,4));
+// console.log(arr.splice(1,4));
+
+
+// const arrr = () => {
+//     debugger
+//     for(let i=0; i<=5; i++){
+//         console.log(i);
+//     }
+// }
+// arrr();
